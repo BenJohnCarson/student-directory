77b0bac6268e5679d1f26fbc9b67ce48e829923a
Benjamin Carson <benjohncarson@gmail.com>
2016-12-05 13:28:05 +0000

    Done exersice 1

--- directory.rb
+++ directory.rb
@@ -23,16 +23,17 @@ def print_header
 end
 
 def print(students)
-    students.each do |student| 
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    students.each_with_index do |student, index| 
+        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
 def print_footer(names)
     puts "Overall, we have #{names.count} great students"
 end
-# Nothing happens untill we call the methods
+
 students = input_students
+# Nothing happens untill we call the methods
 print_header
 print(students)
 print_footer(students)
